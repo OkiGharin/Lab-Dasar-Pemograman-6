f031b62923bdda4eecacdc1666b4537f21eed800
OkiGharin <gharinshauqi9@gmail.com>
2025-12-04 12:15:12 +0700

    Add files via upload

---
+++ Pertemuan 3/Operasi_kondisi.pas
@@ -0,0 +1,71 @@
+uses crt;
+
+var
+    nilai : integer;
+
+begin
+    clrscr;
+
+    write('masukan nilai : ');readln(nilai);
+
+    // // statement if
+    // if (nilai >= 65) then
+    // writeln('anda tidak lulus');
+
+    // statement if else
+    // if (nilai <= 65) then
+    //      begin
+    //         writeln('anda tidak lulus');
+    //     end
+    // else
+    //     begin
+    //         writeln('anda lulus');
+    //     end;
+
+    // statement tiga kondisi
+   
+    // if (nilai = 100) then
+    //     begin
+    //         writeln('anda jago kayak hacker bjorka');
+    //     end
+    // else if (nilai <= 65) then
+    //     begin
+    //         writeln('ulang tahun depan');
+    //     end
+    // else
+    //     begin
+    //         writeln('Anda masuk komunitas');
+    //     end;
+
+    // nested if
+
+    // if (nilai <= 65) then
+    //     begin
+    //         writeln('anda tidak lulus');
+    //     end
+    // else
+    //     begin
+    //         if (nilai = 100) then
+    //             begin
+    //                 writeln('anda lulus dan anda jago');
+    //             end
+    //         else
+    //             begin
+    //                 writeln('anda lulus');
+    //             end
+    //     end;
+
+    // statement case of
+    
+    // case nilai of
+    //     1:writeln('Senin');
+    //     2:writeln('Selasa');
+    //     3:writeln('Rabu');
+    //     4:writeln('Kamis');
+    //     5:writeln('Jumat');
+    //     6:writeln('Sabtu');
+    //     7:writeln('Minggu');
+    // else
+    //     writeln('inputan tidak valid');
+    // end;
+end.
