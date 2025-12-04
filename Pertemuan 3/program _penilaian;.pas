d14c57fcdff287e7336654ea69d9cafc4c38ff44
OkiGharin <gharinshauqi9@gmail.com>
2025-12-04 12:16:19 +0700

    Add files via upload

---
+++ Pertemuan 3/program _penilaian;.pas
@@ -0,0 +1,16 @@
+program penilaian;
+uses crt;
+var   
+kuis, hadir, aktif, tugas, uts, uas, projek, : integer;
+total : real;
+begin
+clrscr;
+    write('Masukan Nilai :');readln(kuis);
+    write('Masukan Nilai :');readln(hadir);
+    write('Masukan Nilai :');readln(aktif);
+    write('Masukan Nilai :');readln(tugas);
+    write('Masukan Nilai :');readln(uts);
+    write('Masukan Nilai :');readln(uas);
+    write('Masukan Nilai :');readln(projek);
+    
+
