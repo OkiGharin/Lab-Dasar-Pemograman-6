4cff205ecd0dbd484c3a9e8bcf901d328544be57
OkiGharin <gharinshauqi9@gmail.com>
2025-12-04 12:25:52 +0700

    Add files via upload

---
+++ Pertemuan 4/uses_crt;pas.pas
@@ -0,0 +1,14 @@
+uses crt;
+var 
+x:array [1..3,1..3] of integer;
+b,k:byte;
+begin 
+    for b:=1 to 3 do 
+        begin   
+    for k:=1 to 3 do 
+        begin 
+            read(x[b,k]);
+    
+     end;
+    end;
+end.
